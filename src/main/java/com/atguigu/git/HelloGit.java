5565fcf60ad8c3d2a0352589a75da8522f2e9c7c
wangyufeng <1037338981@qq.com>
2021-11-06 21:07:04 +0800

    更新1

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -4,7 +4,8 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hello Git!");
+		System.out.println("更新1");
 	}
 
 }
